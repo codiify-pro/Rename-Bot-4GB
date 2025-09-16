f2f39ed5f626cfdd817f9a2cfba606cf0d04a553
Codiify <aaryansah954@gmail.com>
2025-09-16 10:13:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,8 +19,8 @@ LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
 
 
 # Mongo DB Database Config
-DATABASE_URL = os.environ.get("DATABASE_URL", "")
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "madflixbotz")
+DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://gudiyakum678_db_user:4pyIIsSd8F60Fzo9@cluster0.ad5syvd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"")
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "")
 
 
 # Other Variables Config
